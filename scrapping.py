deb2b3a4bc52892187b31cb6984a156c1b9fe856
vhaliabar <vhaliabar@yahoo.com>
2023-03-24 10:44:30 +0200

    refactored script with a clear division into seperate functions

--- scrapping.py
+++ scrapping.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-import telebot
 
 # set up persistent session for login
 session = requests.Session()
-login_url = 'https://www.tesmanian.com/wp-login.php'
-login_data = {
+
+
+# re-authenticate user if response.status
+def auth_my_user():
+    login_url = 'https://www.tesmanian.com/wp-login.php'
+    login_data = {
     'customer[email]': 'chort44@ukr.net',
     'customer[password]': 'wmPj!@4DBS8mPNV',
-}
-session.post(login_url, data=login_data)
+    }
+    return session.post(login_url, data=login_data)
 
 
 # creating function that sends messages to TelegramBot
@@ -22,46 +25,46 @@ def send_to_telegram(message):
 
     try:
         response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
-        print(response.text)
+        #print(response.text)
     except Exception as e:
         print(e)
+
+
+def main():
+    # set up initial variables for previous title and link
+    prev_title = ''
+    prev_link = ''
+    
+    # scraping loop with 15-second interval
+    while True:
+        # send GET request and parse HTML response
+        url = 'https://www.tesmanian.com/blogs/tesmanian-blog'
+        response = session.get(url)
+        if response.status_code != 200:
+            auth_my_user()
+        else:
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # extract title and link of first article
+            articles = soup.find_all('blog-post-card',
+                                     class_='blog-post-card blog-post-card--featured snap-center group rounded bg-secondary')
+            title = articles[0].find('p', class_='h2').text
+            link = articles[0].find('a')['href']
+
+
+            # compare with previous title and link
+            if title != prev_title or link != prev_link:
+                base_url = 'https://www.tesmanian.com'
+                url = base_url+link
+                send_to_telegram(f'New article:\n{title}')
+                send_to_telegram(f'Link:\n{url}')
+
+                # update previous title and link
+                prev_title = title
+                prev_link = link
+
+            # pause script for 15 seconds
+            time.sleep(15)
         
-
-# set up initial variables for previous title and link
-prev_title = ''
-prev_link = ''
-
-
-#taking control over the bot
-#bot = telebot.TeleBot("5989043694:AAE6u6rPQUr6Fd3Ve8Qv4aQ8xKBoy_FOJkA", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
-
-
-# scraping loop with 15-second interval
-while True:
-    # send GET request and parse HTML response
-    url = 'https://www.tesmanian.com/blogs/tesmanian-blog'
-    response = session.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # extract title and link of first article
-    articles = soup.find_all('blog-post-card', class_='blog-post-card blog-post-card--featured snap-center group rounded bg-secondary')
-    #print(articles[0])
-    title = articles[0].find('p', class_='h2').text
-    link = articles[0].find('a')['href']
-
-
-    # compare with previous title and link
-    if title != prev_title or link != prev_link:
-        # print('New article:', title)
-        # print('Link:', link)
-        base_url = 'https://www.tesmanian.com/'
-        url = base_url+link
-        send_to_telegram(f'New article:\n{title}')
-        send_to_telegram(f'Link:\n{url}')
-
-        # update previous title and link
-        prev_title = title
-        prev_link = link
-
-    # pause script for 15 seconds
-    time.sleep(15)
+if __name__ == '__main__':
+    main()
